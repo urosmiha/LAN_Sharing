9c14aa2b9d49a33d5ac03199f5c2a53c9cbb6652
root <mihajlovic.uros.96@gmail.com>
2019-02-05 16:10:48 +1300

    Added intial files/ Added footer, header and index (i.e. main body) of the web page.

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="This is an example. This usually shows in search results.">
+        <meta name-viewport content="width=device-width, initial-scale=1">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="#">
+                    <img src="img/duckling.png" alt="logo">
+                </a>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="index.php">Portfolio</a></li>
+                    <li><a href="index.php">About me</a></li>
+                    <li><a href="index.php">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
